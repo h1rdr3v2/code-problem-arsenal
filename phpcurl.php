1ea12d0ea92c78fbbd6b355d575e4786a1b9aab4
Destiny Ezenwata <ezenwatairoegbu97@gmail.com>
2022-08-07 09:06:08 +0100

    .

--- phpcurl.php
+++ phpcurl.php
@@ -1,6 +1,11 @@
 <?php
     $curl = curl_init();
-
+    $headers = array(
+        'Accept: application/json',
+        'Content-Type: application/json',
+        'Cookie: __test=26973b96c4fe5a82a2b188b2da3f1056'
+        );
+    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);        //Make it reurn data
     curl_setopt($curl, CURLOPT_URL, "http://myundian.rf.gd/public/user/");
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
     $result = curl_exec($curl);
