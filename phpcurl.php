63373c0e0252c37fe9a610f37902e23ae2c31fde
Destiny Ezenwata <ezenwatairoegbu97@gmail.com>
2022-08-07 09:02:24 +0100

    .

--- phpcurl.php
+++ phpcurl.php
@@ -4,6 +4,7 @@
     curl_setopt($curl, CURLOPT_URL, "http://myundian.rf.gd/public/user/");
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
     $result = curl_exec($curl);
-
+    $err = curl_error($curl);
+    echo $err;
     echo ($result);
 ?>
